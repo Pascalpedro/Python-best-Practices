36380f55f9c4e3b9c266333b4565d573a7bfa75d
pascalpedro <Attamapascalpedro@gmail.com>
2023-01-20 21:24:46 +0000

    if-else statement

---
+++ Conditional_Statements/if-else_stat2.py
@@ -0,0 +1,8 @@
+# Program to find the bigger of 2 given numbers....
+
+n1 = int(input("Enter First Number:"))
+n2 = int(input("Enter Second Number:"))
+if n1>n2:
+	print("Bigger Number is:", n1)
+else:
+	print("Bigger Number is:", n2)
