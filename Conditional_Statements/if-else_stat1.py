9a143f071c95b81bb303d8efd84becc9c91d444f
pascalpedro <Attamapascalpedro@gmail.com>
2023-01-20 21:24:35 +0000

    if-else statement

---
+++ Conditional_Statements/if-else_stat1.py
@@ -0,0 +1,10 @@
+# if condition is true, then Action-1 will be executed, otherwise
+# Action-2 will be executed...
+
+name = input("Enter Name:")
+
+if name == "Pedro":
+	print("Hello Pedro, Good Morning.....")
+else:
+	print("Hello Guest, Good Morning...")
+print("How are you!!!")
