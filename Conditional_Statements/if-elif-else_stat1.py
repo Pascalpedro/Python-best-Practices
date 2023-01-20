bc409202bb69ceef06ddbc1f0d12b714b568859d
pascalpedro <Attamapascalpedro@gmail.com>
2023-01-20 21:25:20 +0000

    if-elif-else statement

---
+++ Conditional_Statements/if-elif-else_stat1.py
@@ -0,0 +1,13 @@
+# Based on the conditions, the corresponding action will be executed...
+
+brand = input("Enter your Favourite Brand:")
+
+if brand == "Saloon":
+	print("it's for personal purpose")
+elif brand == "SUV":
+	print("it's for classic purpose")
+elif brand == "Truck":
+	print("it's for work purpose")
+else:
+	print("Others are for general purpose")
+
